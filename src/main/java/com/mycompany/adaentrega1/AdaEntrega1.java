45b0c364059f8cec5969164d0810c65681a9355e
roger <roger@rogerLaptop>
2024-09-20 12:40:43 +0200

    pendiente de revisar pero codigo en teoria terminado

--- src/main/java/com/mycompany/adaentrega1/AdaEntrega1.java
+++ src/main/java/com/mycompany/adaentrega1/AdaEntrega1.java
@@ -18,57 +18,68 @@ import java.io.IOException;
  */
 public class AdaEntrega1 {
 
+    static long asignaciones=0;
+    static long comparaciones=0;
+             
+    
     public static void main(String[] args) {
+        
         int contador;
         int v[];
         Random random = new Random();
-        int asignaciones1[] = new int[10];
-        int asignaciones2[] = new int[10];
-        int asignaciones3[] = new int[10];
+        long asignaciones1[] = new long[10];
+        long asignaciones2[] = new long[10];
+        long asignaciones3[] = new long[10];
 
-        int comparaciones1[] = new int[10];
-        int comparaciones2[] = new int[10];
-        int comparaciones3[] = new int[10];
+        long comparaciones1[] = new long[10];
+        long comparaciones2[] = new long[10];
+        long comparaciones3[] = new long[10];
 
         long tiempo1[] = new long[10];
         long tiempo2[] = new long[10];
         long tiempo3[] = new long[10];
+        
         for (contador = 0; 10 > contador; contador++) {
-            v = new int[(contador + 1) * 10000];
-            int v1[], v2[], v3[], asignaciones=0, comparaciones=0;
+            int tamanio =(contador + 1) * 10000;
+            v = new int[tamanio];
+             aleatorio(v, 10000 * (contador + 1), random);
+            
+            int v1[], v2[], v3[];
+          
+           
             v1 = v.clone();
             v2 = v.clone();
             v3 = v.clone();
 
-            aleatorio(v, 10000 * (contador + 1), random);
-            
+           
             //primera funcion 
             Instant start = Instant.now();
-            ordena1(v1, 10000 * (contador + 1), comparaciones, asignaciones);
+            ordena1(v1, 10000 * (contador + 1));
             Instant finish = Instant.now();
             asignaciones1[contador]=asignaciones;
             comparaciones1[contador]=comparaciones;
             tiempo1[contador] = Duration.between(start, finish).toNanos();
+            System.out.println("Ordena1 Tamaño " + tamanio +" Asignacion "+ asignaciones+ "comparaciones" + comparaciones + " tiempo "+ tiempo1[contador] +"\n");
             //------------------------
             
             //segunda funcion ---
             
-            Instant start = Instant.now();
+            start = Instant.now();
             comparaciones=0;
             asignaciones=0;
-            ordena2(v2, 10000 * (contador + 1), comparaciones, asignaciones);
-            Instant finish = Instant.now();
+            ordena2(v2, 10000 * (contador + 1));
+             finish = Instant.now();
             asignaciones2[contador]=asignaciones;
             comparaciones2[contador]=comparaciones;
             tiempo2[contador] = Duration.between(start, finish).toNanos();
             //---
             
             // tercera funcion ---
-            Instant start = Instant.now();
+             start = Instant.now();
             comparaciones=0;
             asignaciones=0;
-            ordena3(v3, 10000 * (contador + 1), comparaciones, asignaciones);
-            Instant finish = Instant.now();
+            ordena3(v3, 10000 * (contador + 1));
+             finish = Instant.now();
             asignaciones3[contador]=asignaciones;
             comparaciones3[contador]=comparaciones;
             tiempo3[contador] = Duration.between(start, finish).toNanos();
@@ -81,10 +92,10 @@ public class AdaEntrega1 {
         
         generarCSV( comparaciones1, asignaciones1, tiempo1, "ordena1.csv");
         generarCSV( comparaciones2, asignaciones2, tiempo2, "ordena2.csv");
-        generarCSV( comparaciones3, asignaciones1, tiempo3, "ordena3.csv");
+        generarCSV( comparaciones3, asignaciones3, tiempo3, "ordena3.csv");
     }
 
-    public static void ordena1(int v[], int tam, int comparaciones, int asignaciones) {
+    public static void ordena1(int v[], int tam) {
         int i, j, temp;
         i = 1;
         j = 2;
@@ -94,7 +105,8 @@ public class AdaEntrega1 {
                 j = j + 1;
             } else {
                 temp = v[i - 1];
-                v[i - 1] = v[i]; asignaciones +=2; //asignaciones al vector
+                v[i - 1] = v[i]; 
+                asignaciones +=2; //asignaciones al vector
                 v[i] = temp;
                 i = i - 1;
                 if (i == 0) {
@@ -105,49 +117,49 @@ public class AdaEntrega1 {
          comparaciones++;//comparacion if else
        
         }
-
+        System.out.println("termino ordena1");
     }
 
-    public void ordena2(int[] v, int tam,int comparaciones, int asignaciones) {
+    public static void ordena2(int[] v, int tam) {
         int k;
         int n = tam;
         for (k = n / 2; k >= 1; k--) {
-            f(v, k, n,comparaciones, asignaciones);
+            f(v, k, n);
         }
         k = n;
         while (k > 1) {
             g(v, 1, k--);
-            f(v, 1, k,comparaciones,asignaciones);
+            f(v, 1, k);
         }
     }
 
-    private void f(int[] v, int k, int n, int comparaciones, int asignaciones) {
+    private static void f(int[] v, int k, int n) {
         while (2 * k <= n) {
             int j = 2 * k;
             //separar en dos ifs
-            if (j < n)
+            if (j < n){
             	if(v[j - 1] < v[j]) {
                 j++;
             	}
             	comparaciones++;
-            }
+        }
         	comparaciones++;
             if (v[k - 1] >= v[j - 1]) {
                 break;
             }
-            g(v, k, j,comparaciones,asignaciones);
+            g(v, k, j);
             k = j;
         }
     }
 
-    private void g(int[] v, int i, int j, int comparaciones, int asignaciones) {
+    private  static void g(int[] v, int i, int j) {
         int temp = v[i - 1]; 
         v[i - 1] = v[j - 1];
         v[j - 1] = temp;
         asignaciones +=3;
     }
 
-    public void ordena3(int[] v, int tam,int comparaciones, int asignaciones) {
+    public static void ordena3(int[] v, int tam) {
         int m = h(v, tam);
         int[] c = new int[m + 1];
         int[] w = new int[tam];
@@ -166,7 +178,7 @@ public class AdaEntrega1 {
         }
     }
 
-    private int h(int[] v, int tam,int comparaciones int asignaciones) {
+    private static int h(int[] v, int tam) {
         int i;
         int m = v[0]; asignaciones++;
         for (i = 1; i < tam; i++) {
@@ -193,7 +205,7 @@ public class AdaEntrega1 {
         }
     }
 
-    public static void generarCSV( int[] comparaciones, int[] asignaciones, long[] tiempo, String nombreArchivo) {
+    public static void generarCSV( long[] comparaciones, long[] asignaciones, long[] tiempo, String nombreArchivo) {
 
         String archivoCSV = nombreArchivo; // Nombre del archivo de salida
 
